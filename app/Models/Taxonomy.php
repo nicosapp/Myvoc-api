c22e135b524fd93d8d7e1619c9dfa49f025e8167
Niz <62140571+nicosapp@users.noreply.github.com>
2020-11-05 17:50:23 +0100

    fix

--- app/Models/Taxonomy.php
+++ app/Models/Taxonomy.php
@@ -64,16 +64,13 @@ class Taxonomy extends Model
 // INSERT into taxonomies(user_id, taxonomy, name) values(8, 'highlight', '2');
 // INSERT into taxonomies(user_id, taxonomy, name) values(8, 'highlight', '3');
 
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'mode', 'actif');
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'mode', 'passif');
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'mode', 'gérondif');
+
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'tense', 'past');
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'tense', 'present');
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'tense', 'future');
+// INSERT into taxonomies(user_id, taxonomy, name) values(8, 'tense', 'subjunctiv');
+
 // INSERT INTO taxonomy_term(term_id, taxonomy_id) select a.term_id, b.id from grammar_term a, taxonomies b, grammars c where a.grammar_id = c.id and c.name = b.name
-
-//
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'cross_dico', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'rating_1', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'rating_2', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'rating_3', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'rating_4', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'rating_5', NULL, '8', NULL, NULL);
-
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'highlight_1', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'highlight_2', NULL, '8', NULL, NULL);
-// INSERT INTO `taxonomies` (`id`, `taxonomy`, `name`, `order`, `user_id`, `created_at`, `updated_at`) VALUES (NULL, 'color', 'highlight_3', NULL, '8', NULL, NULL);
